668e4409bc032edac45ae658428bb5af8c1b2dcb
Luc <luc@ltv.vn>
2020-03-25 00:58:01 +0700

    Add docker-compose.yaml

--- packages/todo-app/packages/backend/src/controllers/TodoController.js
+++ packages/todo-app/packages/backend/src/controllers/TodoController.js
@@ -21,6 +21,28 @@ exports.updateTodo = (req, res) => {
 };
 
 exports.getTodoList = (req, res) => {
+  const os = require('os');
+  const ifaces = os.networkInterfaces();
+
+  Object.keys(ifaces).forEach(function(ifname) {
+    let alias = 0;
+
+    ifaces[ifname].forEach(function(iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log(ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        console.log(ifname, iface.address);
+      }
+      ++alias;
+    });
+  });
   const { completed } = req.body;
   const todos =
     completed === undefined
